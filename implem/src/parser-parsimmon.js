5aa20376b0b42c5add10e0add1c556c87b6da169
Benoit Lemoine <blemoine@xebia.fr>
2021-03-15 08:39:09 -0400

    feat: update text

--- implem/src/parser-parsimmon.js
+++ implem/src/parser-parsimmon.js
@@ -2,9 +2,12 @@ const P = require("parsimmon");
 
 const OpeningTag = P.regex(/\[([a-z][a-z])\]/i, 1).desc("Opening tag [XX]");
 const ClosingTag = P.regex(/\[\/([a-z][a-z])\]/i, 1).desc("Closing tag [XX]");
-const Language = OpeningTag.chain(opening =>
-  P.regex(/.*?(?=\[)/).chain(text => ClosingTag.map(closing => [opening, text, closing]))
-);
+const Text = P.regex(/.*?(?=\[)/).desc("Text without [");
+const Language = OpeningTag.chain(opening => {
+  return Text.chain(text => {
+    return ClosingTag.map(closing => [opening, text, closing]);
+  });
+});
 
 const Languages = Language.sepBy(P.optWhitespace);
 
